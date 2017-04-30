81870661bf48af11163c57d3c68505833da7cee7
cocotyty <cocotyty@sina.com>
2017-04-30 16:59:47 +0800

    add MustMap

--- default.go
+++ default.go
@@ -9,6 +9,14 @@ func ScanDir(dirPath string) (e error) {
 func Map(objs ...interface{}) error {
 	return DefaultManager.Map(objs...)
 }
+func MustMap(obj interface{}) {
+	err := Map(obj)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
 func AddFunc(name string, fnFactory func(ctx *FnCtx) (fn TemplateFunc)) {
 	DefaultManager.AddFunc(name, fnFactory)
 }
