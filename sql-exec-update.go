f18e1f39300b033973e02c9bd6312cb9c1a3146c
cocotyty <cocotyty@sina.com>
2017-05-08 10:58:42 +0800

    fix bugs

--- sql-exec-update.go
+++ sql-exec-update.go
@@ -5,22 +5,28 @@ import "reflect"
 func (s *SQLExecutor) Execute(args []reflect.Value) (results []reflect.Value) {
 	sqlstring, sqlargs, err := s.executeTpl(args)
 	if err != nil {
-		return []reflect.Value{
-			reflect.Zero(s.ReturnTypes[0]),
-			reflect.ValueOf(&err).Elem(),
-		}
+		return s.returnError(err)
 	}
 	rs, err := s.DB.Exec(sqlstring, sqlargs...)
 	if err != nil {
-		return []reflect.Value{
-			reflect.Zero(s.ReturnTypes[0]),
-			reflect.ValueOf(&err).Elem(),
-		}
+		return s.returnError(err)
 	}
 	var emptyError error
 	affected, _ := rs.RowsAffected()
-	return []reflect.Value{
-		reflect.ValueOf(affected),
-		reflect.ValueOf(&emptyError).Elem(),
+	if s.ReturnTypes[0].Kind() == reflect.Int64 {
+		return []reflect.Value{
+			reflect.ValueOf(affected),
+			reflect.ValueOf(&emptyError).Elem(),
+		}
 	}
+	if s.ReturnTypes[0].Kind() == reflect.Int {
+		return []reflect.Value{
+			reflect.ValueOf(int(affected)),
+			reflect.ValueOf(&emptyError).Elem(),
+		}
+	}
+	if len(s.ReturnTypes) == 1 {
+		return s.returnError(emptyError)
+	}
+	panic("NOT SUPPORT")
 }
